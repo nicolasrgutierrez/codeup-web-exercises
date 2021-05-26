d513fd30ae07318729ab645721724e47b5498bed
Nicolas Gutierrez <nicolas.rene.gutierrez@gmail.com>
2021-05-26 12:25:04 -0500

    Started exercises

---
+++ js/external.js
@@ -0,0 +1,7 @@
+console.log("Hello from external JavaScript");
+
+    alert('Welcome to my Website!');
+
+var userInput = prompt('What is your favorite color?');
+    alert(userInput + ' is my favorite color too!');
+
