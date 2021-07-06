39745b83ed0c8bcb3b785129e2a8ee369a0804fb
Nicolas Gutierrez <nicolas.rene.gutierrez@gmail.com>
2021-07-06 09:31:27 -0500

    incomplete morning warmups

--- js/morningWarmUps.js
+++ js/morningWarmUps.js
@@ -10,7 +10,23 @@ function countSpaces(str) {
     }
 }
 
+// July 6th 2021
+// TODO: Create a function named capitalizeAllNames that accepts an array of strings and returns the array with all of its elements capitalized.
+// Example:  capitalizeAllNames(["bob","Seth","TOFU","Toyota"]) ---- returns ----> ["BOB","SETH","TOFU","TOYOTA"]
 
+function capitalizeAllNames() {
+    let array = ["bob","Seth","TOFU","Toyota"]
+ return capitalizeAllNames().toUpperCase();
+}
+console.log(capitalizeAllNames())
+// TODO: Create a function named capitalizeFirstLetter that accepts an array of strings and returns the array with all of the first letters in
+//  each string being capitalized while having the rest of the string lowercase look at the example below for reference.
+
+function capitalizeFirstLetter() {
+
+}
+
+// Example:  capitalizeFirstLetter(["bob","Seth","TOFU","Toyota"]) ---- returns ----> ["Bob","Seth","Tofu","Toyota"]
 
 
 
