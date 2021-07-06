1a231472219c986b5523412d57f5aa36e749714f
Nicolas Gutierrez <nicolas.rene.gutierrez@gmail.com>
2021-07-06 11:41:28 -0500

    warmup exercise git add .

--- js/morningWarmUps.js
+++ js/morningWarmUps.js
@@ -11,20 +11,31 @@ function countSpaces(str) {
 }
 
 // July 6th 2021
-// TODO: Create a function named capitalizeAllNames that accepts an array of strings and returns the array with all of its elements capitalized.
+//  Create a function named capitalizeAllNames that accepts an array of strings and returns the array with all of its elements capitalized.
 // Example:  capitalizeAllNames(["bob","Seth","TOFU","Toyota"]) ---- returns ----> ["BOB","SETH","TOFU","TOYOTA"]
-
-function capitalizeAllNames() {
-    let array = ["bob","Seth","TOFU","Toyota"]
- return capitalizeAllNames().toUpperCase();
+//Use a for or for each loop
+function capitalizeAllNames(stringArray) {
+    stringArray = ["bob","Seth","TOFU","Toyota"];
+    for (let i = 0; i < stringArray.length; i++) {
+        stringArray[i] = stringArray[i].toUpperCase();
+    }
+    console.log(capitalizeAllNames(stringArray))
 }
-console.log(capitalizeAllNames())
-// TODO: Create a function named capitalizeFirstLetter that accepts an array of strings and returns the array with all of the first letters in
+//  Create a function named capitalizeFirstLetter that accepts an array of strings and returns the array with all of the first letters in
 //  each string being capitalized while having the rest of the string lowercase look at the example below for reference.
 
-function capitalizeFirstLetter() {
+function capitalizeFirstLetter(stringsArray) {
+    var capitalizedArray = [];
 
+    stringsArray.forEach(function(string) {
+        var capitalized = string[0].toUpperCase() + string.slice(1).toLowerCase();
+        capitalizedArray.push(capitalized)
+    });
+
+    return capitalizedArray;
 }
+console.log('capitalized');
+console.log(capitalizeFirstLetter(["bob","Seth","TOFU","Toyota"]));
 
 // Example:  capitalizeFirstLetter(["bob","Seth","TOFU","Toyota"]) ---- returns ----> ["Bob","Seth","Tofu","Toyota"]
 
