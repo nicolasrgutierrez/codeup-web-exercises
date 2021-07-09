19b06e792cfbb32f95ed8b4d60567629613c14ce
Nicolas Gutierrez <nicolas.rene.gutierrez@gmail.com>
2021-07-09 14:29:18 -0500

    started jQuery selectors exercise

--- js/jquery-exercises.js
+++ js/jquery-exercises.js
@@ -1,3 +1,23 @@
 "use strict";
+/*
+$(document).ready(function() {
 
+    h1 = $("h1")
 
+    console.log(h1.html("Swiper was here"))
+
+    alert('I am ready for manipulation!');
+
+     aDiv = $("#something")
+
+    console.log(aDiv)
+
+    var divs = $(".content")
+
+    console.log(divs)
+});
+*/
+
+$(document).ready(function() {
+
+});
