82d85c15a192fe22aa1b73b50daad437bc961b75
Nicolas Gutierrez <nicolas.rene.gutierrez@gmail.com>
2021-07-22 16:00:46 -0500

    started map, filter, reduce exercise

---
+++ js/map-filter-reduce.js
@@ -0,0 +1,58 @@
+"use strict";
+
+const users = [
+    {
+        id: 1,
+        name: 'ryan',
+        email: 'ryan@codeup.com',
+        languages: ['clojure', 'javascript'],
+        yearsOfExperience: 5
+    },
+    {
+        id: 2,
+        name: 'luis',
+        email: 'luis@codeup.com',
+        languages: ['java', 'scala', 'php'],
+        yearsOfExperience: 6
+    },
+    {
+        id: 3,
+        name: 'zach',
+        email: 'zach@codeup.com',
+        languages: ['javascript', 'bash'],
+        yearsOfExperience: 7
+    },
+    {
+        id: 4,
+        name: 'fernando',
+        email: 'fernando@codeup.com',
+        languages: ['java', 'php', 'sql'],
+        yearsOfExperience: 8
+    },
+    {
+        id: 5,
+        name: 'justin',
+        email: 'justin@codeup.com',
+        languages: ['html', 'css', 'javascript', 'php'],
+        yearsOfExperience: 9
+    }
+];
+
+const threeLanguageUsers = users.filter(function (user) {
+    return user.languages.length >= 3
+})
+console.log(threeLanguageUsers);
+
+const emailString = users.map(function (users) {
+    return users.email
+})
+console.log(emailString);
+
+const totalYears = users.reduce((total, users) => {
+    return total + users.yearsOfExperience;
+}, 0);
+console.log(totalYears);
+
+const avgYears = users.reduce(function (users) {
+
+})
